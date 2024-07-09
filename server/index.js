9169bdb18e06e92f5edfa316f4e9d986738a90a9
Dalida Assan <dalidaassan@MacBook-Air-9.local>
2024-07-09 17:25:36 +0500

    fixed base url

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   const start = Date.now();
   res.on('finish', () => {
     const duration = Date.now() - start;
-    console.log(`${req.method} ${req.baseUrl} ${res.statusCode} - ${duration}ms`);
+    console.log(`${req.method} ${req.url} ${res.statusCode} - ${duration}ms`);
   });
   next();
 });
