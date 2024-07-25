8fbbb0199e6ee6fc013fbe6656a472f8530dbceb
Dalida Assan <dalidaassan@MacBook-Air-9.local>
2024-07-25 23:36:56 +0500

    auth changes

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,5 +1,4 @@
 const JwtStrategy = require('passport-jwt').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
@@ -21,34 +20,5 @@ module.exports = passport => {
         })
         .catch(err => console.log(err));
     })
-  );
-
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_REDIRECT_URI,
-      },
-      (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-          .then(user => {
-            if (user) {
-              return done(null, user);
-            } else {
-              const newUser = new User({
-                googleId: profile.id,
-                name: profile.displayName,
-                email: profile.emails[0].value,
-              });
-
-              newUser.save()
-                .then(user => done(null, user))
-                .catch(err => console.log(err));
-            }
-          })
-          .catch(err => console.log(err));
-      }
-    )
   );
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,30 +3,12 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const { google } = require('googleapis');
 const User = require('../models/User');
 const authMiddleware = require('../middlewares/auth-middleware');
 
 require('dotenv').config();
 const secretOrKey = process.env.JWT_SECRET;
 
-const googleConfig = {
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  redirect: process.env.GOOGLE_REDIRECT_URI
-};
-
-const oauth2Client = new google.auth.OAuth2(
-  googleConfig.clientId,
-  googleConfig.clientSecret,
-  googleConfig.redirect
-);
-
-const scopes = [
-  'https://www.googleapis.com/auth/plus.me',
-  'https://www.googleapis.com/auth/userinfo.email',
-  'https://www.googleapis.com/auth/userinfo.profile'
-];
 
 // Регистрация
 router.post('/register', (req, res) => {
@@ -98,22 +80,6 @@ router.get('/me', authMiddleware, (req, res) => {
     email: req.user.email,
     region: req.user.region
   });
-});
-
-// Google OAuth2
-router.get('/google', (req, res) => {
-  const url = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: scopes
-  });
-  res.redirect(url);
-});
-
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-  const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, {
-    expiresIn: 360000
-  });
-  res.redirect(`http://localhost:3000/dashboard?token=${token}`);
 });
 
 module.exports = router;
